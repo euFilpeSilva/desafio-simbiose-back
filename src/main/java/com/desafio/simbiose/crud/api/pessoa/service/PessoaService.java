01946db02f4a536d18be26563507c8302a3cadf6
euFilpeSilva <sousadasilvafilipe@gmail.com>
2023-08-29 14:27:53 -0300

    refactor/tratamento de erro nos serviços que faltaram

--- src/main/java/com/desafio/simbiose/crud/api/pessoa/service/PessoaService.java
+++ src/main/java/com/desafio/simbiose/crud/api/pessoa/service/PessoaService.java
@@ -6,6 +6,7 @@ import com.desafio.simbiose.crud.api.pessoa.repository.PessoaRepository;
 import com.desafio.simbiose.crud.api.pessoa.web.controller.mapper.PessoaMapper;
 import com.desafio.simbiose.crud.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -16,24 +17,38 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class PessoaService {
 
+    private static final String NOT_FOUND = "Dados não encontrados na base de dados";
+    private static final String NOT_DELETE = "Falha na execução da operação";
+    private static final String NOT_SALV = "Erro ao salvar ou atualizar a pessoa.";
+
     private final PessoaRepository repository;
 
     private final PessoaMapper mapper;
 
     public void salvaOuAtualizaPessoa(PessoaDto dto) {
-
-        if (Optional.ofNullable(dto.getId()).isPresent()) {
-            Pessoa pessoa = mapper.toDtoPessoa(dto);
-            repository.save(pessoa);
-        } else {
-            repository.save(mapper.toDtoPessoa(dto));
+        try {
+            if (Optional.ofNullable(dto.getId()).isPresent()) {
+                Pessoa pessoa = mapper.toDtoPessoa(dto);
+                repository.save(pessoa);
+            } else {
+                repository.save(mapper.toDtoPessoa(dto));
+            }
+        } catch (Exception e) {
+            throw new BusinessException(NOT_SALV);
         }
     }
 
+
     public Page<PessoaDto> listarPessoas(Pageable page) {
         Page<Pessoa> pessoasPage = repository.findAll(page);
+
+        if (pessoasPage.isEmpty()) {
+            throw new BusinessException(NOT_FOUND);
+        }
+
         return pessoasPage.map(mapper::toPessoaDto);
     }
+
 
     public PessoaDto buscarPorId(String id) {
         Optional<Pessoa> pessoaOptional = repository.findById(id);
@@ -42,11 +57,16 @@ public class PessoaService {
             Pessoa pessoa = pessoaOptional.get();
             return mapper.toPessoaDto(pessoa);
         } else {
-            throw new BusinessException("Pessoa não encontrada na base de dados");
+            throw new BusinessException(NOT_FOUND);
         }
     }
 
     public void deletarPorId(String id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new BusinessException(NOT_DELETE);
+        }
     }
+
 }
